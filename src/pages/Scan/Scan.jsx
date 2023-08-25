586eb75fdb18a8f4ae06c84e62dece2fe6206769
anagelberg <amy.nagelberg@gmail.com>
2023-08-25 01:59:49 -0700

    try camera again

--- src/pages/Scan/Scan.jsx
+++ src/pages/Scan/Scan.jsx
@@ -9,7 +9,7 @@ import { useState } from "react";
 
 const videoConstraints = {
   facingMode: { exact: "environment" }, //toggle "user" "environment"
-  // width: 428,
+  width: window.innerWidth,
   height: window.innerHeight,
 };
 
