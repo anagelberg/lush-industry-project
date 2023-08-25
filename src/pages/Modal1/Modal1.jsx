645d6ca88e4f9f0a5958ffbbe732b8303569050d
anagelberg <amy.nagelberg@gmail.com>
2023-08-25 11:52:35 -0700

    build password form

--- src/pages/Modal1/Modal1.jsx
+++ src/pages/Modal1/Modal1.jsx
@@ -1,17 +1,48 @@
 //See receipt button
 import modal1 from "../../assets/page-shots/modal1.svg";
+import "./Modal1.scss";
 import { useNavigate } from "react-router";
+import Button from "../../components/Button/Button";
+import CharInput from "../../components/CharInput/CharInput";
+import { useState } from "react";
 
 function Modal1() {
   const navigate = useNavigate();
+
+  const totalInputs = 6;
+  const [index, setIndex] = useState(1);
+
+  const handleChange = (value, index) => {
+    console.log(`Input ${index} changed to ${value}`);
+    setIndex((prev) => prev + 1);
+  };
+
   return (
-    <div
-      className="page-screenshot"
-      onClick={() => {
-        navigate("/modal2");
-      }}
-    >
-      <img src={modal1} alt="" />
+    <div className="absolute-container">
+      <div className="page-screenshot">
+        <img src={modal1} alt="" />
+        <div className="password-container">
+          {Array.from({ length: totalInputs }).map((_, index) => (
+            <CharInput
+              key={index}
+              index={index}
+              onChange={handleChange}
+              total={totalInputs}
+            />
+          ))}
+        </div>
+
+        <div className="button-absolute">
+          <Button
+            disabled={index <= totalInputs}
+            modifier="--secondary"
+            text="proceed to checkout"
+            onClick={() => {
+              navigate("/checkout1");
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
