c9f2959543474505ff64bbf88ab9cac95cce23b0
Elise <eliselimin@outlook.com>
2023-08-25 12:50:33 -0700

    preserved this version for merge to main

--- src/pages/BasketPage/BasketPage.jsx
+++ src/pages/BasketPage/BasketPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "../../components/Button/Button";
 import "./BasketPage.scss";
 import item from "../../assets/images/Itemized.png";
@@ -8,9 +8,31 @@ import { useNavigate } from "react-router";
 import addIcon from "../../assets/icons/carbon_add-filled.svg"
 import minusIcon from "../../assets/icons/zondicons_minus-solid.svg"
 
+
 function BasketPage() {
   const navigate = useNavigate("/");
-  
+  const [quantity, setQuantity] = useState (1);
+  const [price, setPrice] = useState (8.25);
+  const [tax, setTax] = useState (0.99); 
+  const [total, setTotal] = useState (9.24); 
+const handleAdd = () => {
+    setQuantity(prevQty => prevQty + 1) 
+    setPrice(prePrice => prePrice+ 8.25)
+
+}
+
+const handleMinus = () => {
+    setQuantity(quantity - 1)
+    setPrice(price - 8.25) 
+
+}
+
+useEffect(() => {
+    setTax(Number(Number(price * 0.12).toFixed(2)))
+    setTotal (price + tax)
+}, [price, tax])
+
+
   return (
     <>
       <div className="progress-bar">
@@ -20,9 +42,9 @@ function BasketPage() {
           alt="Progress Bar - Step 1 - Basket"
         />
       </div>
-      <div className="title">My Shopping Basket (1)</div>
+      <div className="title">My Shopping Basket ({quantity})</div>
       <div className="checkout-item">
-        <div>
+        <div className="checkout-item__image-wrapper">
         <img
           className="checkout-item__image"
           src={item}
@@ -31,11 +53,11 @@ function BasketPage() {
         </div>
         <div className="quantity-wrapper">
         <div className="quantity">
-            <img className="quantity__addicon" src={addIcon} alt="Add Icon"/>
-            <p className="quantity__number">1</p>
-            <img className="quantity__minusicon"src={minusIcon} alt="Minus Icon"/>
+            <img onClick={handleAdd} className="quantity__addicon" src={addIcon} alt="Add Icon"/>
+            <p className="quantity__number">{quantity}</p>
+            <img onClick={handleMinus} className="quantity__minusicon"src={minusIcon} alt="Minus Icon"/>
         </div>
-        <div className="quantity__price">CA$8.00</div>
+        <div className="quantity__price">CA${price}</div>
       </div>
       </div>
       <div className="add-item">
@@ -43,15 +65,15 @@ function BasketPage() {
       </div>
       <div className="subtotal">
         <div className="subtotal__text">Subtotal</div>
-        <div className="subtotal__number">CA$8.00</div>
+        <div className="subtotal__number">CA${price}</div>
       </div>
       <div className="tax">
         <div className="tax__text">Tax</div>
-        <div className="tax__number">CA$0.96</div>
+        <div className="tax__number">CA${tax}</div>
       </div>
       <div className="total">
         <div className="total__text">Total</div>
-        <div className="total__number">CA$9.68</div>
+        <div className="total__number">CA${total}</div>
       </div>
       <div className="button">
         <Button
